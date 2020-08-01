79d4ca344dd32155095170a29b2f86d009a003a9
Parsa Hosseini <officer47p@gmail.com>
2020-08-01 23:42:32 +0430

    now the orders screen uses future builder to show the loading spinner while the data is being fetched, also end of the http requests module

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -13,44 +13,90 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  List<or.OrderItem> orders = [];
-  bool _isInit = true;
-  bool _isLoading = false;
+  // List<or.OrderItem> orders = [];
+  // bool _isInit = true;
+  // bool _isLoading = false;
 
-  @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      loadResources();
-      _isInit = false;
-    }
-    super.didChangeDependencies();
-  }
+  // @override
+  // void didChangeDependencies() {
+  //   if (_isInit) {
+  //     loadResources();
+  //     _isInit = false;
+  //   }
+  //   super.didChangeDependencies();
+  // }
 
-  Future<void> loadResources() async {
-    final ordersData = Provider.of<or.Orders>(context);
-    setState(() {
-      _isLoading = true;
-    });
-    await ordersData.fetchAndSetOrders();
-    setState(() {
-      orders = ordersData.orders;
-      _isLoading = false;
-    });
-  }
+  // Future<void> loadResources() async {
+  //   final ordersData = Provider.of<or.Orders>(context);
+  //   setState(() {
+  //     _isLoading = true;
+  //   });
+  //   await ordersData.fetchAndSetOrders();
+  //   setState(() {
+  //     orders = ordersData.orders;
+  //     _isLoading = false;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
+    print("calling rebuild");
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Orders"),
-      ),
-      drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: orders.length,
-              itemBuilder: (context, i) => OrderItem(orders[i]),
-            ),
-    );
+        appBar: AppBar(
+          title: Text("Orders"),
+        ),
+        drawer: AppDrawer(),
+        body: FutureBuilder(
+          builder: (ctx, futureSnapShot) {
+            if (futureSnapShot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (futureSnapShot.error == null) {
+                print("Getting the orders");
+                // final orders = Provider.of<or.Orders>(context).orders;
+                return Consumer<or.Orders>(
+                  builder: (context, value, child) => ListView.builder(
+                    itemCount: value.orders.length,
+                    itemBuilder: (context, i) => OrderItem(value.orders[i]),
+                  ),
+                );
+                //     ListView.builder(
+                //   itemBuilder: (ctx, i) {
+                //     return OrderItem(orders[i]);
+                //   },
+                //   itemCount: orders.length,
+                // );
+              } else {
+                return Center(
+                  child: Text("Couldn't load the data"),
+                );
+              }
+            }
+          },
+          future: Provider.of<or.Orders>(context, listen: false)
+              .fetchAndSetOrders(),
+        )
+
+        // _isLoading
+        //     ? Center(child: CircularProgressIndicator())
+        //     : ListView.builder(
+        //         itemCount: orders.length,
+        //         itemBuilder: (context, i) => OrderItem(orders[i]),
+        //       ),
+        );
   }
 }
+// class OrdersListView extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final orders = Provider.of<or.Orders>(context, listen: false).orders;
+//     return ListView.builder(
+//       itemBuilder: (ctx, i) {
+//         return OrderItem(orders[i]);
+//       },
+//       itemCount: orders.length,
+//     );
+//   }
+// }
