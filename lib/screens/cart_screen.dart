3a8792331cea6ae8c1d8280c1b6a2973e9643e58
Parsa Hosseini <officer47p@gmail.com>
2020-08-01 13:14:44 +0430

    cart screen: show circular progress indicator instead of the order now text, added a gesture detector to prevent the user from going back

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -19,102 +19,110 @@ class _CartScreenState extends State<CartScreen> {
     final cart = Provider.of<Cart>(context);
     final ctx = context;
     // final scaffold = Scaffold.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Cart"),
-      ),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Column(
-              children: <Widget>[
-                Card(
-                  margin: EdgeInsets.all(15),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Total",
-                          style: TextStyle(fontSize: 20),
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+          appBar: AppBar(
+            title: Text("Cart"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Card(
+                margin: EdgeInsets.all(15),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Total",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Spacer(),
+                      Chip(
+                        label: Text(
+                          "\$${cart.calculateTotalPrice.toString()}",
+                          style: TextStyle(color: Colors.white),
                         ),
-                        Spacer(),
-                        Chip(
-                          label: Text(
-                            "\$${cart.calculateTotalPrice.toString()}",
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          backgroundColor: Theme.of(context).primaryColor,
-                        ),
-                        FlatButton(
-                          child: Text(
-                            "ORDER NOW",
-                            style: TextStyle(fontWeight: FontWeight.w400),
-                          ),
-                          textColor: Theme.of(context).primaryColor,
-                          onPressed: cart.itemCount > 0
-                              ? () async {
+                        backgroundColor: Theme.of(context).primaryColor,
+                      ),
+                      FlatButton(
+                        child: _isLoading
+                            ? Center(
+                                child: CircularProgressIndicator(),
+                              )
+                            : Text(
+                                "ORDER NOW",
+                                style: TextStyle(fontWeight: FontWeight.w400),
+                              ),
+                        textColor: Theme.of(context).primaryColor,
+                        onPressed: cart.itemCount > 0
+                            ? () async {
+                                setState(() {
+                                  _isLoading = true;
+                                });
+                                try {
+                                  await Provider.of<Orders>(context,
+                                          listen: false)
+                                      .addOrder(
+                                    products: cart.items.values.toList(),
+                                    amount: cart.calculateTotalPrice,
+                                  );
+                                  cart.clearCart();
+                                } catch (err) {
+                                  await showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                      title: Text("An error accourd"),
+                                      content: Text("Something went wrong"),
+                                      actions: <Widget>[
+                                        FlatButton(
+                                          child: Text("Ok"),
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(),
+                                        )
+                                      ],
+                                    ),
+                                  );
+                                } finally {
                                   setState(() {
-                                    _isLoading = true;
+                                    _isLoading = false;
                                   });
-                                  try {
-                                    await Provider.of<Orders>(context,
-                                            listen: false)
-                                        .addOrder(
-                                      products: cart.items.values.toList(),
-                                      amount: cart.calculateTotalPrice,
-                                    );
-                                    cart.clearCart();
-                                  } catch (err) {
-                                    await showDialog(
-                                      context: context,
-                                      builder: (context) => AlertDialog(
-                                        title: Text("An error accourd"),
-                                        content: Text("Something went wrong"),
-                                        actions: <Widget>[
-                                          FlatButton(
-                                            child: Text("Ok"),
-                                            onPressed: () =>
-                                                Navigator.of(context).pop(),
-                                          )
-                                        ],
-                                      ),
-                                    );
-                                  } finally {
-                                    setState(() {
-                                      _isLoading = false;
-                                    });
-                                    Navigator.of(context).pop();
-                                  }
+                                  Navigator.of(context).pop();
                                 }
-                              : null,
-                        )
-                      ],
-                    ),
+                              }
+                            : null,
+                      )
+                    ],
                   ),
                 ),
-                SizedBox(
-                  height: 10,
-                ),
-                Expanded(
-                    child: ListView.builder(
-                  itemBuilder: (ctx, i) {
-                    final item = cart.items.entries.toList()[i];
-                    print("Rebuilding CartItem ${item.key}");
-                    return CartItem(
-                      id: item.key,
-                      price: item.value.price,
-                      quantity: item.value.quantity,
-                      titel: item.value.title,
-                      deleteItem: cart.removeItem,
-                    );
-                  },
-                  itemCount: cart.items.length,
-                )),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                itemBuilder: (ctx, i) {
+                  final item = cart.items.entries.toList()[i];
+                  print("Rebuilding CartItem ${item.key}");
+                  return CartItem(
+                    id: item.key,
+                    price: item.value.price,
+                    quantity: item.value.quantity,
+                    titel: item.value.title,
+                    deleteItem: cart.removeItem,
+                  );
+                },
+                itemCount: cart.items.length,
+              )),
+            ],
+          ),
+        ),
+        if (_isLoading)
+          GestureDetector(
+            onTap: () {},
+          ),
+      ],
     );
   }
 }
